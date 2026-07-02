6c6bf649cf685b4b4ef6cecf4dc9fd662ac76017
java-SG <carlo_van_der_sloot@hotmail.com>
2026-07-02 15:43:55 +0200

    Realigned with other project, which subdivided smaller methods for readibility

--- src/Application/ParseExcel.java
+++ src/Application/ParseExcel.java
@@ -3,111 +3,118 @@ package Application;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+
+import static Application.Utility.getFile;
 
 public class ParseExcel {
 
-    private static Object extractCellValue(Cell cell) {
-
-        if (cell == null) return null;
-
-        /* Currently parses only:
-        * Strings
-        * Booleans
-        * Numerics -> and Dates from Numerics
-        *  */
-
-        return switch (cell.getCellType()) {
-            /* todo:
-                Expand switch cases in case more CellTypes become relevant,
-                see: enum list CellType in CellType.class (.getCellType() -> CellType -> enum list))
-            */
-            case STRING -> cell.getStringCellValue().trim(); /* Trim leading and trailing spaces, typical for entity name restrictions */
-            /* todo:
-                Implement dynamic way to apply enum typing to specific model enums, e.g.:
-                case STRING -> {
-                    if (header.equals("gender")) {
-                        String value = cell.getStringCellValue().trim().toUpperCase();
-                        yield Gender.GenderNames.valueOf(value);
-                    } else { yield cell.getStringCellValue();}
-                } // -> This would now apply the enum type of an enum list defined in Model "Gender" containing the enum list "GenderNames"
-            */
-            case BOOLEAN -> cell.getBooleanCellValue();
-            case NUMERIC -> {
-                if (DateUtil.isCellDateFormatted(cell)) {
-                    yield cell.getLocalDateTimeCellValue().toLocalDate();
-                    /* todo:
-                        Expand this when dates with time are relevant
-                    */
-                }
-                yield cell.getNumericCellValue();
-            }
-
-            default -> null;
-
-        };
-    }
-
     public static Map<String, List<Map<String, Object>>> parseExcel(String filename) throws Exception {
 
-        /* Import static getFile() if placed in separate packages, getFile() will only work when finding one and exactly one matching file */
-        Path path = Path.of(Utility.getFile(filename));
-        System.out.println("Parsed file -> " + path);
-        InputStream stream = Files.newInputStream(path);
-        Map<String, List<Map<String, Object>>> data = new LinkedHashMap<>();
+        String filePath = getFile(filename);
+        System.out.println("Parsed file -> " + filePath);
 
-        try (Workbook workbook = new XSSFWorkbook(stream)) {
+        return buildData(filePath);
 
-//            DataFormatter formatter = new DataFormatter(); // Useful but not necessary for basic use
+    }
+
+    private static Map<String, List<Map<String, Object>>> buildData(String filePath) {
+        Map<String, List<Map<String, Object>>> excelData = new LinkedHashMap<>();
+
+        try (Workbook workbook = new XSSFWorkbook(new FileInputStream(filePath))) {
+
+            DataFormatter formatter = new DataFormatter();
 
             for (int currentSheet = 0; currentSheet < workbook.getNumberOfSheets(); currentSheet++) {
 
                 Sheet sheet = workbook.getSheetAt(currentSheet);
-                String sheetName = sheet.getSheetName();
-                List<Map<String, Object>> sheetData = new ArrayList<>();
-                Row headerRow = sheet.getRow(0);
+                Row headers = sheet.getRow(0);
 
-                if (headerRow == null) continue; /* Continue to next sheet when no more headers are found */
+                if (headers == null) continue;
 
                 /* Build headers */
-                List<String> headers = new ArrayList<>();
-                for (int currentCell = 0; currentCell < headerRow.getLastCellNum(); currentCell++) {
-                    Cell cell = headerRow.getCell(currentCell);
-                    headers.add(cell != null ? cell.toString().trim() : "");
-                }
+                List<String> keys = buildKeys(headers, formatter);
 
-                /* Create list of entity objects */
-                for (int currentRow = 1; currentRow <= sheet.getLastRowNum(); currentRow++) {
-                    Row row = sheet.getRow(currentRow);
+                /* Create list of entity objects and add to sheet */
+                List<Map<String, Object>> entries = buildEntries(sheet, keys);
 
-                    if (row == null) continue;
-
-                    /* Create entity object */
-                    Map<String, Object> entityEntry = new LinkedHashMap<>();
-
-                    for (int indexHeader = 0; indexHeader < headers.size(); indexHeader++) {
-                        String header = headers.get(indexHeader); /* Get correct header as key */
-                        Object cell = extractCellValue(row.getCell(indexHeader)); /* Extract cell value as Cell, then formatted as Object */
-                        entityEntry.put(header, cell); /* Add each "key=value" to current entry for building an entity object */
-                    }
-
-                    sheetData.add(entityEntry); /* Add entity object to list of entity objects */
-
-                }
-
-                data.put(sheetName, sheetData); /* Add sheetname with list of entity objects to excel data */
+                /* Add sheet with mapped entries */
+                excelData.put(sheet.getSheetName(), entries);
 
             }
+
+        } catch (IOException exception) {
+
+            throw new RuntimeException(exception);
+
         }
 
-        return data;
+        return excelData;
 
     }
 
+    private static List<String> buildKeys(Row headerRow, DataFormatter formatter) {
+        List<String> keyList = new ArrayList<>();
+        for (int currentCell = 0; currentCell < headerRow.getLastCellNum(); currentCell++) {
+            Cell cell = headerRow.getCell(currentCell);
+            keyList.add(cell != null ? formatter.formatCellValue(cell).trim() : "");
+        }
+        return keyList;
+    }
+
+    private static List<Map<String, Object>> buildEntries(Sheet sheet, List<String> keys) {
+        List<Map<String, Object>> sheetEntries = new ArrayList<>();
+        for (int row = 1; row <= sheet.getLastRowNum(); row++) {
+
+            Row currentRow = sheet.getRow(row);
+            if (currentRow == null) continue;
+
+            Map<String, Object> entityEntry = new LinkedHashMap<>();
+
+            if (!buildEntry(entityEntry, keys, currentRow)) {
+                break; /* If entityEntry only has null values, skip .add() */
+            }
+            ;
+            sheetEntries.add(entityEntry);
+        }
+        return sheetEntries;
+    }
+
+    private static boolean buildEntry(Map<String, Object> entityEntry, List<String> keys, Row row) {
+        for (int currentCell = 0; currentCell < keys.size(); currentCell++) {
+            String key = keys.get(currentCell);
+            Object value = extractCellValue(key, row.getCell(currentCell));
+            if (!key.isEmpty()) {
+                entityEntry.put(key, value); /* Skip column in case of missing header */
+            }
+        }
+        return entityEntry.values().stream().anyMatch(Objects::nonNull);
+    }
+
+    private static Object extractCellValue(String header, Cell cell) {
+
+        if (cell == null) return null;
+
+        /* Currently parses only:
+         * Strings
+         * Booleans
+         * Numerics -> and Dates from Numerics
+         *  */
+
+        return switch (cell.getCellType()) {
+            /* todo: Expand switch cases in case more CellTypes become relevant, see: enum list CellType in CellType.class (.getCellType() -> CellType -> enum list)) */
+            case STRING -> cell.getStringCellValue();
+            case BOOLEAN -> cell.getBooleanCellValue();
+            case NUMERIC -> {
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    yield cell.getLocalDateTimeCellValue().toLocalDate();
+                    /* todo: Expand this when dates with time are relevant */
+                }
+                yield cell.getNumericCellValue();
+            }
+            default -> null;
+        };
+    }
 }
